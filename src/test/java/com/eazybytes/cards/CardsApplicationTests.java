f58d5846e604e9ac05205211576cea2d6d6761a8
shubham kumar <shubhamkmuz@gmail.com>
2026-03-12 18:28:42 +0530

    Added changes to use spring bus refresh to fetch config data

--- src/test/java/com/eazybytes/cards/CardsApplicationTests.java
+++ src/test/java/com/eazybytes/cards/CardsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.eazybytes.cards;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CardsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.eazybytes.cards;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CardsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
